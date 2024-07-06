69510cf2878f5ebf5496b19a30513e87c03ae6cc
Tania Kuzmenko <tanch.sergeevich@gmail.com>
2024-07-06 23:04:16 +0300

    make build

---
+++ index.02b6f745.js
@@ -0,0 +1,2 @@
+var r=document.querySelector("table");!function(){var e=r.rows,t=!0,l=!1,n=void 0;try{for(var o,a=e[Symbol.iterator]();!(t=(o=a.next()).done);t=!0){var i=o.value,c=i.cells;if(c.length>1){var f=c[1].cloneNode(!0);i.insertBefore(f,c[c.length-1])}}}catch(r){l=!0,n=r}finally{try{t||null==a.return||a.return()}finally{if(l)throw n}}}();
+//# sourceMappingURL=index.02b6f745.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const table = document.querySelector("table");
+function cloneSecondColumn() {
+    const rows = table.rows;
+    for (const row of rows){
+        const cells = row.cells;
+        if (cells.length > 1) {
+            const clonedCell = cells[1].cloneNode(true);
+            row.insertBefore(clonedCell, cells[cells.length - 1]);
+        }
+    }
+}
+cloneSecondColumn();
+
+//# sourceMappingURL=index.f75de5e1.js.map
